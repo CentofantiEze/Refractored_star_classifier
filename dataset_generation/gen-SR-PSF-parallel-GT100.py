1e56ab75a5730b7b7d181a02108b1afde100e535
CentofantiEze <ezecentofanti@gmail.com>
2024-03-14 14:39:03 +0100

    Dataset 4 generation.

--- dataset_generation/gen-SR-PSF-parallel-GT100.py
+++ dataset_generation/gen-SR-PSF-parallel-GT100.py
@@ -45,8 +45,8 @@ dataset_id = 4
 dataset_id_str = '%03d'%(dataset_id)
 
 # This list must be in order from bigger to smaller
-n_star_list = [10000]
-n_test_stars = 2000
+n_star_list = [52000]
+n_test_stars = 20000
 # Total stars
 n_stars = n_star_list[0] + n_test_stars
 # Max train stars
